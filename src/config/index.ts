5da5bb7d57952e85a4daafefd78ceac5dbd9dd56
Manuel Maldonado <manuelmaldonado2898@gmail.com>
2026-04-24 18:42:40 -0400

    Fix lectura del socket

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
 //export const SERVER_ROUTE = (import.meta as any).env?.VITE_API_URL ?? 'http://localhost:4809';
-export const SERVER_ROUTE = (import.meta as any).env?.VITE_API_URL ?? ' http://api.chuys.tech';
+export const SERVER_ROUTE = (import.meta as any).env?.VITE_API_URL ?? 'http://api.chuys.tech';
